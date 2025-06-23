c78ecbc7bdd2de5e999553dd0f76e10f4be99c5d
KamalMusayev <kmusayev038@gmail.com>
2025-06-23 19:25:21 +0400

    Update 3-square.py

--- python-classes/3-square.py
+++ python-classes/3-square.py
@@ -5,12 +5,13 @@
 class Square:
     """This is a Class."""
 
-    
-    def __init__(self, size = 0):
+    def __init__(self, size=0):
         self.size = size
+
     @property
     def size(self):
         return self.__size
+
     @size.setter
     def size(self, value):
         if not isinstance(value, int):
@@ -18,5 +19,6 @@ class Square:
         if value < 0:
             raise ValueError("size must be >= 0")
         self.__size = value
+
     def area(self):
         return self.__size ** 2
