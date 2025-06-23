43d69afb6ba1ac191852127fc7fa8a74e88b34cb
KamalMusayev <kmusayev038@gmail.com>
2025-06-23 19:23:02 +0400

    Update 2-square.py

--- python-classes/2-square.py
+++ python-classes/2-square.py
@@ -3,17 +3,13 @@
 
 
 class Square:
-    """This is a Class"""
+    """This is a class."""
 
-    
-    def __init__(self, size = 0):
-
+    def __init__(self, size=0):
         if not isinstance(size, int):
             raise TypeError("size must be an integer")
-
         if size < 0:
             raise ValueError("size must be >= 0")
-
         self.__size = size
 
     def area(self):
