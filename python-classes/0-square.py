a03885a88420836c62983e61d3b819d80ac0b027
KamalMusayev <kmusayev038@gmail.com>
2025-06-23 19:17:35 +0400

    Update 0-square.py

--- python-classes/0-square.py
+++ python-classes/0-square.py
@@ -1,14 +1,6 @@
 #!/usr/bin/python3
-"""A module that defines a square."""
-class Square:
-    """Represents a square.
-    This class defines a square by a private instance attribute 'size'.
-    """
-    def __init__(self, size):
-        """Initializes a new instance of the Square class.
-        Args:
-            size: The size of the square. It has no type or value
-                  verification as per the requirements.
-        """
-        self.__size = size
 
+
+class Square:
+    def __init__(self, size):
+        self.__size = size
